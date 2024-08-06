04c84a8a93d254c0bb19466daf8f2bb6008a3ea5
Saloni Sharma <mailsalonisharma@gmail.com>
2024-08-05 22:06:20 -0400

    event planner changes

---
+++ event_planner-main/src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+    import React from 'react';
+    const Footer = () => {
+            return (
+        <>
+        <footer>
+            <div>
+                <p>All rights reserved 2024</p>
+            </div>
+        </footer>
+        </>
+    )}
+
+export default Footer
